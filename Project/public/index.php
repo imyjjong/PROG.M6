b8059883c691ee5d7723882bde5b2c91d4625f4d
Sara <34916@ma-web.nl>
2023-11-29 12:57:36 +0100

    opdracht 8.5

--- Project/public/index.php
+++ Project/public/index.php
@@ -3,12 +3,12 @@ require_once '../source/config.php';
 require_once SOURCE_ROOT . 'database.php';
 
 $connection = database_connect();
-$amsterdam = 'amsterdam';
+$plaats = 'amsterdam';
 $haarlem = 'haarlem';
 $almere = 'almere';
 $sql = 'SELECT * FROM weersomstandighedenPerDag WHERE plaats=? OR plaats=? OR plaats=? ORDER BY datum';
 $stmt = $connection->prepare($sql);
-$stmt->bind_param('sss', $amsterdam, $haarlem, $almere);
+$stmt->bind_param('sss', $plaats, $haarlem, $almere);
 $stmt->execute();
 $result = $stmt->get_result();
 $weersomstandigheden = mysqli_fetch_all($result);
--- Project/public/search.php
+++ Project/public/search.php
@@ -1,5 +1,28 @@
 <?php
-    echo "<pre>";
-    var_dump($_GET['search']);
-    echo "</pre>";
+    require_once '../source/config.php';
+    require_once SOURCE_ROOT . 'database.php';
+
+    $connection = database_connect();
+    $plaats = $_GET['search'];
+    $sql = 'SELECT * FROM weersomstandighedenPerDag WHERE plaats=? ORDER BY datum';
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('s', $plaats);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while($weersomstandigheid = mysqli_fetch_assoc($result)){
+        $datum = $weersomstandigheid['datum'];
+        $plek = $weersomstandigheid['plaats'];
+        $graden = $weersomstandigheid['graden'];
+        $windkracht = $weersomstandigheid['windkracht'];
+        $regen = $weersomstandigheid['regenInMilimeters'];
+        echo '<link rel="stylesheet" href="assets/css/style.css">';
+        echo "<body>";
+        echo "<pre>";
+        echo "<p class='plaats'>".$plaats ." ". $datum . "</p>";
+        echo "<p>Graden: ".$graden ."</p>";
+        echo "<p>Windkracht: ".$windkracht ."</p>";
+        echo "<p>Regen: ".$regen . "mm</p>";
+        echo "</pre>";
+        echo "</body>";
+     }
 ?>
