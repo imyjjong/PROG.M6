86f6e8f9641afcbc4e3faafa17c5fe2566175dde
Sara <34916@ma-web.nl>
2023-12-07 10:13:20 +0100

    haha foutje

---
+++ Project/public/assets/js/appFetch.js
@@ -0,0 +1,20 @@
+let getForm = document.getElementById("getForm");
+
+getForm.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    toPhpWitGet(event);
+});
+
+function toPhpWitGet(){
+    let form = event.target;
+    const data = new FormData(form);
+
+    console.log(data.get("article"));
+    console.log(data.get("maxPrice"));
+
+    let url = "fetchGet.php?article="+data.get("article")+"&maxPrice="+data.get("maxPrice");
+    fetch(url)
+    .then((response)=>{
+        console.log(response);
+    });
+}
