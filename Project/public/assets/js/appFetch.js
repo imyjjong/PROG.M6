f3450d8f42791521d46960a6f44cd916146fd3fc
Sara <34916@ma-web.nl>
2023-12-07 13:20:17 +0100

    opdracht 3.4

--- Project/public/assets/js/appFetch.js
+++ Project/public/assets/js/appFetch.js
@@ -2,10 +2,10 @@ let getForm = document.getElementById("getForm");
 
 getForm.addEventListener("submit", (event) =>{
     event.preventDefault();
-    toPhpWitGet(event);
+    toPhpWithGet(event);
 });
 
-function toPhpWitGet(){
+function toPhpWithGet(event){
     let form = event.target;
     const data = new FormData(form);
 
@@ -17,4 +17,38 @@ function toPhpWitGet(){
     .then((response)=>{
         console.log(response);
     });
-}
+};
+
+let postForm = document.getElementById("postForm");
+
+postForm.addEventListener("submit", (event => {
+    event.preventDefault();
+    toPhpWithPost(event);
+}));
+
+function FormToDictionary(form){
+    const data = new FormData(form);
+    let formKeyValue = {};
+    for(const [name,value] of data){
+        formKeyValue[name] = value;
+    }
+    return formKeyValue;
+};
+
+function toPhpWithPost(event){
+    let form = event.target;
+    let jsonForm = FormToDictionary(form);
+    console.log(jsonForm);
+
+    let options = 
+    {
+        method: "POST",
+        cache: "no-cache",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(jsonForm)
+    };
+    fetch("fetchPost.php", options)
+    .then(async(response)=>{
+        console.log(response);
+    });
+};
