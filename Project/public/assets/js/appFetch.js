e7c7c04be915d23a611647dc9362cdb4ef4747d7
Sara <34916@ma-web.nl>
2023-12-16 13:48:08 +0100

    opdracht 4.4

--- Project/public/assets/js/appFetch.js
+++ Project/public/assets/js/appFetch.js
@@ -65,9 +65,20 @@ function searchPersoon(event){
         console.log(response);
         let json = await response.json();
         console.log(json);
+        showPerson(json);
     });
 };
 
 searchFormulier.addEventListener("submit", (event) =>{
     searchPersoon(event);
 });
+
+function showPerson(json){
+    let person = json[0];
+    document.getElementById("nawItem__id").textContent = person.id;
+    document.getElementById("nawItem__name").textContent = person.naam;
+    document.getElementById("nawItem__street").textContent = person.straat;
+    document.getElementById("nawItem__huisnr").textContent = person.huisnummer;
+    document.getElementById("nawItem__postcode").textContent = person.postcode;
+    document.getElementById("nawItem__email").textContent = person.email;
+};
