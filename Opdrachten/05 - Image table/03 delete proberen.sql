bb1dd627ca6968047bbfef8286473c95f1c19e1a
Sara <34916@ma-web.nl>
2023-12-22 21:31:57 +0100

    opdracht 5.3

---
+++ Opdrachten/05 - Image table/03 delete proberen.sql
@@ -0,0 +1,2 @@
+DELETE FROM `imagetable` WHERE id =1;
+DELETE FROM `imagetable` WHERE name ="lizrex";
---
+++ Opdrachten/05 - Image table/03 image insert.sql
@@ -0,0 +1,6 @@
+USE `m6prog`;
+INSERT INTO `imagetable`(`name`, `path`, `type`, `tmp_name`, `error`, `size`)
+VALUES
+("lizelizarafael", "lizelizarafael.png", "image/png", "/tmp/phpeAHkJO", 0, 35697),
+("lizrex", "lizrex.png", "image/png", "/tmp/phpBHcoMa", 0, 52733),
+("lizsdrr", "lizsdrr.png", "image/png", "/tmp/phpgeMCoE", 0, 31843);
