160a0679d7ade8620ef2707af93277b33e9634f6
Sara <34916@ma-web.nl>
2023-12-23 16:11:27 +0100

    opdracht 6.1

--- Project/public/imagerecieve.php
+++ Project/public/imagerecieve.php
@@ -1,4 +1,24 @@
 <?php
-    move_uploaded_file($_FILES["image"]["tmp_name"],"../uploads/temp.png");
     print_r($_FILES);
+
+    function handleFile($file){
+    };
+    $response = ["succeeded" => false, "message" => ""];
+    
+    $file = $_FILES["image"];
+    if($file["error"] == 0){
+        $response["succeeded"] = handleFile($file);
+    }
+    else{
+        $response["message"] = "error during upload ". $file["error"];
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($response);
+
+    $link = uniqid();
+    $location = $file["tmp_name"];
+    $ext = ".png";
+    $filename = "../uploads/$link$ext";
+    move_uploaded_file($location, $filename);
 ?>
